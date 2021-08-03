98eb1a431f89b39e57a318f43f1c2743200030f3
Linhav2000 <tranlinhvl20@gmail.com>
2021-08-03 16:14:21 +0700

    tạo user,xóa hết và dữ lại

---
+++ test3/CUser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo useradd -d /home/$1 $1
+
+#if [ $1 == 'delete_user']
+#then
+#	sudo userdel demo1 
+#fi
+
---
+++ test3/DUser1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo userdel -r $1 # xóa hết không dữ lại 
